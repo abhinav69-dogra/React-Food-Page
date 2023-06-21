2eaf7b91e6e2da19d3a9a8b8659414b1f220b2e1
Ayush Raj <rajrakshal20@gmail.com>
2023-06-21 12:47:13 +0530

    Time for the test

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import Header from "../Header";
+
+test("Logo should load on rendering header", () => {
+  // load header
+  const header = render(<Header />);
+
+  // check if logo is loaded ?
+});
