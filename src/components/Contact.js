8fb9aecf656da2b44b6cd59d0297e413995f2033
Abhinav Dogra <65779864+abhinav69-dogra@users.noreply.github.com>
2023-07-13 16:57:18 +0530

    project setup done

--- src/components/Contact.js
+++
@@ -1,9 +0,0 @@
-const Contact = () => {
-  return (
-    <div>
-      <h1>Contact Us Page 📞</h1>
-    </div>
-  );
-};
-
-export default Contact;
--- src/components/__tests__/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Header from "../Header";
-
-test("Logo should load on rendering header", () => {
-  // load header
-  const header = render(<Header />);
-
-  // check if logo is loaded ?
-});
