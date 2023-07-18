1f905814f02e79cb6e0c16db01754c53925f2de9
Abhinav Dogra <65779864+abhinav69-dogra@users.noreply.github.com>
2023-07-18 18:02:40 +0530

    added hooks and filter btn

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,34 @@
 import restaurantList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
+
   return (
-    <div className="restaurant-list">
-      {restaurantList.map((restaurant) => {
-        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-      })}
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {listOfRestaurant.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} {...restaurant.data} />
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Body; 
-
+export default Body;
